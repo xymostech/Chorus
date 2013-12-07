20f8833ae921d9b66103bb607e8822756c904bde
root <root@Chorus.(none)>
2013-12-07 14:18:48 -0500

    first capture commit.

---
+++ capture.js
@@ -0,0 +1,33 @@
+var pcap = require('pcap'),
+	tcp_tracker = new pcap.TCP_tracker(),
+	pcap_session = pcap.createSession('', "ip proto \\tcp");
+var events = require('events');
+//Chorus objects to be emmitted
+function chorus_obj() {
+	
+}
+//Session objects, event emmitter
+function session_obj() {
+	
+}
+session_obj.prototype.__proto__ = events.EventEmitter.prototype;
+
+var capture = new session_obj();
+
+tcp_tracker.on('start', function (session) {
+	//console.log('START: ' + session.src_name + '->' + session.dst_name);
+	
+});
+tcp_tracker.on('end', function (session) {
+	//console.log('END: ' + session.src_name + '->' + session.dst_name);
+	console.log(JSON.stringify(session));
+});
+tcp_tracker.on('http error', function (session) {
+	
+});
+
+
+pcap_session.on('packet', function (raw_packet) {
+	var packet = pcap.decode.packet(raw_packet);
+	tcp_tracker.track_packet(packet);
+});
